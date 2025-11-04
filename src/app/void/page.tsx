1c734d12dbf50f9f55d94328a01af6171321a7eb
Vignesh Narayanan <vignarayanan04@gmail.com>
2025-11-04 07:40:05 +0000

    Reduce the spacing between the 4 states and filter card by 2 units (_for

--- src/app/void/page.tsx
+++ src/app/void/page.tsx
@@ -16,10 +16,10 @@ export default function VoidPage() {
       <SidebarInset>
         <div className="flex flex-col h-screen">
           <AppHeader />
-          <div className="p-4 md:px-6 md:pt-6">
+          <div className="p-4 pb-2 md:px-6 md:pt-6">
             <StatusToolbar statuses={statusCards} />
           </div>
-          <div className="border-b p-4 md:px-6">
+          <div className="border-b p-4 pt-2 md:px-6">
             <Button variant="outline" size="sm" className="flex items-center gap-2">
                 <Filter className="h-4 w-4" />
                 <span>Filter</span>
