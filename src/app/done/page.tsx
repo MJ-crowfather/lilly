5428438cf3a47b50196e092645fd4fc802515a11
Vignesh Narayanan <vignarayanan04@gmail.com>
2025-11-04 13:46:08 +0000

    No, okay listen carefully, I want the filter and everything above it to

--- src/app/done/page.tsx
+++ src/app/done/page.tsx
@@ -31,12 +31,12 @@ export default function DonePage() {
       <SidebarInset>
         <div className="flex flex-col h-screen">
           <AppHeader />
-          <main className="flex-1 flex flex-col p-4 md:px-6">
-            <div className="rounded-lg border bg-card text-card-foreground shadow-sm">
-                <div className="p-4">
-                    <StatusToolbar statuses={statusCards} />
-                </div>
-                <div className="border-t p-4 flex items-center justify-between">
+          <main className="flex-1 flex flex-col">
+            <div className="p-4 md:px-6">
+                <StatusToolbar statuses={statusCards} />
+            </div>
+            <div className="rounded-lg border-t bg-card text-card-foreground shadow-sm mx-4 md:mx-6">
+                <div className="p-4 flex items-center justify-between">
                     <Button variant="outline" size="sm">
                         <Filter className="h-4 w-4 mr-2" />
                         Filter
@@ -45,36 +45,36 @@ export default function DonePage() {
                         <ListFilter className="h-4 w-4" />
                     </Button>
                 </div>
-                <div className="border-t">
-                    <div className="relative w-full overflow-auto">
-                        <Table>
-                            <TableHeader>
-                                <TableRow>
-                                    {tableHeaders.map((header) => (
-                                        <TableHead key={header}>{header}</TableHead>
-                                    ))}
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                {doneCases.map((caseItem) => (
-                                    <TableRow key={caseItem.case_number}>
-                                        <TableCell className="font-medium">{caseItem.case_number}</TableCell>
-                                        <TableCell>{caseItem.receipt_date}</TableCell>
-                                        <TableCell>{caseItem.social_network}</TableCell>
-                                        <TableCell>{caseItem.username}</TableCell>
-                                        <TableCell>{caseItem.lilly_product}</TableCell>
-                                        <TableCell>{caseItem.report_type}</TableCell>
-                                        <TableCell>{caseItem.respondent_type}</TableCell>
-                                        <TableCell>{caseItem.country}</TableCell>
-                                        <TableCell className="max-w-[250px] truncate">{caseItem.case_summary}</TableCell>
-                                        <TableCell>{caseItem.case_status}</TableCell>
-                                        <TableCell>{caseItem.processing_time}</TableCell>
-                                        <TableCell>{caseItem.assigned_agent}</TableCell>
-                                    </TableRow>
+            </div>
+            <div className="flex-1 overflow-auto px-4 md:px-6 pb-4">
+                <div className="border-x border-b rounded-b-lg bg-card text-card-foreground">
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                {tableHeaders.map((header) => (
+                                    <TableHead key={header}>{header}</TableHead>
                                 ))}
-                            </TableBody>
-                        </Table>
-                    </div>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {doneCases.map((caseItem) => (
+                                <TableRow key={caseItem.case_number}>
+                                    <TableCell className="font-medium">{caseItem.case_number}</TableCell>
+                                    <TableCell>{caseItem.receipt_date}</TableCell>
+                                    <TableCell>{caseItem.social_network}</TableCell>
+                                    <TableCell>{caseItem.username}</TableCell>
+                                    <TableCell>{caseItem.lilly_product}</TableCell>
+                                    <TableCell>{caseItem.report_type}</TableCell>
+                                    <TableCell>{caseItem.respondent_type}</TableCell>
+                                    <TableCell>{caseItem.country}</TableCell>
+                                    <TableCell className="max-w-[250px] truncate">{caseItem.case_summary}</TableCell>
+                                    <TableCell>{caseItem.case_status}</TableCell>
+                                    <TableCell>{caseItem.processing_time}</TableCell>
+                                    <TableCell>{caseItem.assigned_agent}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
                 </div>
             </div>
           </main>
