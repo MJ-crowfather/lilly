ade485b7fbdc63bc81c168c862fe50a0eae0cd47
Vignesh Narayanan <vignarayanan04@gmail.com>
2025-11-06 13:46:52 +0000

    Awesome now some small tweaks here, remove all the colons : (_for elemen

--- src/components/reconciliation/key-details.tsx
+++ src/components/reconciliation/key-details.tsx
@@ -45,19 +45,19 @@ export const KeyDetails = ({ caseData, artifacts }: { caseData: ClutchDoneCase, 
                         <div className="space-y-3 text-sm">
                             <h4 className="font-medium text-muted-foreground text-xs">Summary</h4>
                              <div className="grid grid-cols-[max-content,1fr] gap-x-2 gap-y-2 text-xs">
-                                <span className="text-muted-foreground">Customer Name:</span>
+                                <span className="text-muted-foreground">Customer Name</span>
                                 <span className="font-medium text-foreground text-left">{caseData.customer_full_name}</span>
                                 
-                                <span className="text-muted-foreground">Vehicle:</span>
+                                <span className="text-muted-foreground">Vehicle</span>
                                 <span className="font-medium text-foreground text-left">{caseData.vehicle_year} {caseData.vehicle_make} {caseData.vehicle_model}</span>
                                 
-                                <span className="text-muted-foreground">BOS Effective Date:</span>
+                                <span className="text-muted-foreground">BOS Effective Date</span>
                                 <span className="font-medium text-foreground text-left">{caseData.bos_effective_date}</span>
 
-                                <span className="text-muted-foreground">Applicable Loan / Lien Amount:</span>
+                                <span className="text-muted-foreground">Applicable Loan / Lien Amount</span>
                                 <span className="font-medium text-foreground text-left">{caseData.applicable_loan_balance}</span>
                                 
-                                <span className="text-muted-foreground">Net Vehicle Value:</span>
+                                <span className="text-muted-foreground">Net Vehicle Value</span>
                                 <span className="font-medium text-foreground text-left">${caseData.net_vehicle_value.toFixed(2)}</span>
                             </div>
                         </div>
