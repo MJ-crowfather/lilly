399a7df481f2c12819aff52558edf9cd67c0f893
Vignesh Narayanan <vignarayanan04@gmail.com>
2025-11-06 13:31:25 +0000

    make the dark green frames for the boxes darker and thicker throughout w

--- src/components/reconciliation/done-status-icon.tsx
+++ src/components/reconciliation/done-status-icon.tsx
@@ -4,6 +4,6 @@ import * as React from 'react';
 
 export const DoneStatusIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <rect width="8" height="8" rx="1.5" className="fill-green-100 dark:fill-green-900/50 stroke-green-600 dark:stroke-green-400" strokeWidth="1"/>
+      <rect x="0.75" y="0.75" width="6.5" height="6.5" rx="2" className="fill-green-100 dark:fill-green-900/50 stroke-green-700 dark:stroke-green-400" strokeWidth="1.5"/>
     </svg>
 );
